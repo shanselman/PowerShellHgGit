19a4082b1f2a4e704a40db018923210a8beb8fb9
Ashhar Hasan <hashhar_dev@outlook.com>
2015-11-17 04:17:30 +0530

    Reformatted code and prettified it

--- prompt.ps1
+++ prompt.ps1
@@ -7,20 +7,20 @@ $thePromptHash = [Convert]::ToBase64String($md5.ComputeHash($thePrompt))
 
 if ($thePromptHash -eq $defaultPromptHash) #using the default prompt?
 {
-    #recommend our own
-    function prompt(){
-        # Reset color, which can be messed up by Enable-GitColors
-        $Host.UI.RawUI.ForegroundColor = $GitPromptSettings.DefaultForegroundColor
+	#recommend our own
+	function prompt() {
+		# Reset color, which can be messed up by Enable-GitColors
+		$Host.UI.RawUI.ForegroundColor = $GitPromptSettings.DefaultForegroundColor
 
-        Write-Host($pwd) -nonewline -foregroundcolor white
+		Write-Host($pwd) -nonewline -foregroundcolor white
 
-        Write-VcsStatus 
+		Write-VcsStatus
 
-        Write-Host ""
-        return "$ "
-    }
+		Write-Host ""
+		return "$ "
+	}
 }
 else {
-    Write-Debug "Make sure your prompt includes a called to Write-VcsStatus!"    
+	Write-Debug "Make sure your prompt includes a call to Write-VcsStatus!"
 }
 
